d5cb539b7b0cb8504d371c95ebc74cb7cb0a8e3c
OrBibi <Orbibiariel@gmail.com>
2024-11-21 16:59:54 +0200

    change the disc dountion

--- OOP_EX1_2025/GreedyAI.java
+++ OOP_EX1_2025/GreedyAI.java
@@ -16,23 +16,19 @@ public class GreedyAI extends AIPlayer{
                 allPositions.add(new Position(i, j));
             }
         }
-        Comparator<Position> flipComparator = new Comparator<Position>() {
-            @Override
-            public int compare(Position p1, Position p2) {
-                int flips1 = gameStatus.countFlips(p1);
-                int flips2 = gameStatus.countFlips(p2);
-
-                // First comparing by flips:
-                if (flips1 != flips2) {
-                    return Integer.compare(flips2, flips1);
+        Comparator<Position> flipComparator = (p1, p2) -> {
+            // First comparing by flips:
+            int flips1 = gameStatus.countFlips(p1);
+            int flips2 = gameStatus.countFlips(p2);
+            if (flips1 != flips2) {
+                return Integer.compare(flips2, flips1);
+            } else {
+                // If the flips are equal: comparing by column
+                if (p1.col() != p2.col()) {
+                    return Integer.compare(p2.col(), p1.col());
                 } else {
-                    // If the flips are equal: comparing by column
-                    if (p1.col() != p2.col()) {
-                        return Integer.compare(p2.col(), p1.col());
-                    } else {
-                        // If the flips and column are equal: comparing by row
-                        return Integer.compare(p2.row(), p1.row());
-                    }
+                    // If the flips and column are equal: comparing by row
+                    return Integer.compare(p2.row(), p1.row());
                 }
             }
         };
@@ -40,7 +36,6 @@ public class GreedyAI extends AIPlayer{
         allPositions.sort(flipComparator);
         Position bestMove = allPositions.getFirst();
         Disc disc = new SimpleDisc(this);
-        Move move = new Move(bestMove, disc);
-        return move;
+        return new Move(bestMove, disc);
     }
 }
